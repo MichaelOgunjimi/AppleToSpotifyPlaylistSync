43ed14a095ec16c0bc1177fad372a26df64cf626
Michael Ogunjimi <michael_Ogunjimi@yahoo.com>
2023-06-28 06:35:02 +0100

    Add and updated some
    This add a function to delete playlists and also to get user playlists, then I amended some function to fit it purpose

--- SpotifyPlaylistManager.py
+++ SpotifyPlaylistManager.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -6,6 +8,7 @@ class SpotifyPlaylistManager:
     """
     Spotify Playlist Manager
     """
+
     def __init__(self, client_id, client_secret, redirect_uri, cache_path):
         self.sp = spotipy.Spotify(
             auth_manager=SpotifyOAuth(
@@ -30,6 +33,19 @@ class SpotifyPlaylistManager:
         playlist = self.sp.user_playlist_create(user=self.user_id, name=name, public=public, description=description)
         return playlist['id']
 
+    def user_playlists(self):
+        """
+        This function gets the user playlists
+        :return:
+        """
+        playlists = self.sp.user_playlists(self.user_id)
+        playlist_ids = []
+        # Iterate through each playlist and print its name
+        for playlist in playlists['items'][:-2]:
+            print(playlist['name'])
+            playlist_ids.append(playlist['id'])
+        return playlist_ids
+
     def upload_playlist_cover_image(self, playlist_id, image_b64):
         """
         This function uploads the playlist cover image
@@ -41,19 +57,44 @@ class SpotifyPlaylistManager:
 
     def search_track_uri(self, song, artist):
         """
-        This function searches for a song in Spotify with the information gotten from Apple Music Website
-        :param song:
-        :param artist:
-        :return uri of the song:
+        This function searches for a song in Spotify with the information obtained from the Apple Music website
+        :param song: Song title
+        :param artist: Artist name
+        :return: URI of the song if found, else None
         """
-        result = self.sp.search(f"track:{song} artist:{artist}", type="track")
         try:
-            uri = result["tracks"]["items"][0]["uri"]
-            print('Song found')
-            return uri
+            search_query = f"track:{song} artist:{artist}"
+            result = self.sp.search(search_query, type="track")
+
+            if result["tracks"]["items"]:
+                uri = result["tracks"]["items"][0]["uri"]
+                print('Song found')
+                return uri
         except IndexError:
-            print(f"{song} doesn't exist in Spotify. Skipped.")
-            return None
+            # Retry search without artist
+            try:
+                search_query_without_artist = f"track:{song}"
+                result_without_artist = self.sp.search(search_query_without_artist, type="track")
+                if result_without_artist["tracks"]["items"]:
+                    for item in result_without_artist['tracks']['items'][:5]:
+                        song_title = item['name']
+                        artist_name = item['artists'][0]['name']
+                        print(song_title, artist_name)
+
+                index = int(input(
+                    f"Which song do you want to add? from 0 to {len(result_without_artist['tracks']['items'][:5])}: if song doesn't exist, enter -1: "))
+                if index == -1 or index > 4:
+                    return None
+                uri_without_artist = result_without_artist["tracks"]["items"][index]["uri"]
+                print('Song found without artist')
+                print(uri_without_artist)
+                return uri_without_artist
+            except IndexError:
+                print(f"{song} doesn't exist in Spotify. Skipped.")
+                return None
+
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+        return None
 
     def add_tracks_to_playlist(self, playlist_id, track_uris):
         """
@@ -62,7 +103,18 @@ class SpotifyPlaylistManager:
         :param track_uris:
         :return:
         """
-        self.sp.user_playlist_add_tracks(user=self.user_id, playlist_id=playlist_id, tracks=track_uris)
+        if track_uris is None:
+            print("Error: track_uris parameter is None")
+            return
+
+        # Filter out None values from track_uris list
+        track_uris = [uri for uri in track_uris if uri is not None]
+
+        try:
+            self.sp.user_playlist_add_tracks(user=self.user_id, playlist_id=playlist_id, tracks=track_uris)
+            print("Tracks added successfully")
+        except Exception as e:
+            print(f"Error: Failed to add tracks to playlist - {e}")
 
     def update_tracks_in_playlist(self, playlist_id, track_uris):
         """
@@ -72,3 +124,12 @@ class SpotifyPlaylistManager:
         :return: 
         """
         self.sp.user_playlist_replace_tracks(user=self.user_id, playlist_id=playlist_id, tracks=track_uris)
+
+    def delete_playlist(self, playlist_id):
+        """
+        This function deletes a playlist
+        :param playlist_id:
+        :return:
+        """
+        self.sp.user_playlist_unfollow(user=self.user_id, playlist_id=playlist_id)
+        print(f"Deleted playlist with ID: {playlist_id}")
